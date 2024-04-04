251fcb7cdf9f38dafa30d19f5c5453ea4b770c4a
nhan.tran <nhan.tran@asnet.com.vn>
2024-04-04 13:22:24 +0700

    Update Breadcrumbs component

--- src/app/ui/components/common/Breadcrumbs/index.tsx
+++ src/app/ui/components/common/Breadcrumbs/index.tsx
@@ -1,26 +1,42 @@
+"use client";
+
 import Link from "next/link";
 import { memo } from "react";
+import { usePathname } from "next/navigation";
+const isEqual = require("react-fast-compare");
 
 interface Breadcrumb {
   label: string;
   href: string;
 }
 
-const Breadcrumbs = ({ breadcrumbs }: { breadcrumbs: Breadcrumb[] }) => (
-  <nav aria-label="Breadcrumb" className="text-xs uppercase text-gray-100 pt-2">
-    <ol className="flex">
-      {breadcrumbs.map(({ href, label }, index) => {
-        const lastItem = breadcrumbs.length - 1;
+const Breadcrumbs = ({ breadcrumbs }: { breadcrumbs: Breadcrumb[] }) => {
+  const pathname = usePathname();
 
-        return (
-          <li key={href} aria-current={!!lastItem}>
-            <Link href={href}>{label}</Link>
-            {index < breadcrumbs.length - 1 && <span className="mx-1">/</span>}
-          </li>
-        );
-      })}
-    </ol>
-  </nav>
-);
+  return (
+    <nav
+      aria-label="Breadcrumb"
+      className="text-xs uppercase text-gray-100 pt-2">
+      <ol className="flex">
+        {breadcrumbs.map(({ href, label }, index) => {
+          const isActive = pathname.includes(href);
 
-export default memo(Breadcrumbs);
+          return (
+            <li key={href} aria-current={isActive}>
+              <Link
+                className={`${isActive ? "text-blue-600" : ""}`}
+                href={href}>
+                {label}
+              </Link>
+              {index < breadcrumbs.length - 1 && (
+                <span className="mx-1">/</span>
+              )}
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
+  );
+};
+
+export default memo(Breadcrumbs, isEqual);
